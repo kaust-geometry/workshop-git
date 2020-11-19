17a693a570f9849c25041001ce5714de6a515159
Pieter Barendrecht <pjbarendrecht@gmail.com>
2020-11-19 10:25:50 +0300

    Initial commit, added Python file

---
+++ compute_cosine.py
@@ -0,0 +1,6 @@
+from math import pi, cos
+
+degrees = input("Please provide an angle (in degrees): ")
+radians = degrees * pi / 180
+
+print("The cosine of the user-provided angle is ", cos(radians), sep = '')
