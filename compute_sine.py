e378dbc3c936a474444b50b85be36e0763da0cfd
Victor Ceballos <VictorCeballos@users.noreply.github.com>
2020-11-19 10:00:36 +0100

    Update and rename compute_cosine.py to compute_sine.py

--- compute_sine.py
+++ compute_sine.py
@@ -3,4 +3,4 @@ from math import pi, cos
 degrees = float(input("Please provide an angle (in degrees): "))
 radians = degrees * pi / 180
 
-print("The cosine of the user-provided angle is ", cos(radians), sep = '')
+print("The sine of the user-provided angle is ", sin(radians), sep = '')
